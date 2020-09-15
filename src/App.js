caf35136bdff4e2e3c472f6882db66bdc48fc1fc
pairofAces <karanchauhan16@gmail.com>
2020-09-15 14:35:32 -0400

    Understand the lectures but I'm lost here...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import './App.css';
+import { yes, no } from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+  // console.log({yes, no})
+
+  // state = {
+  //   shown: true
+  // }
+
+  clickHandler = () => {
+    console.log("clicked")
+  }
+
+  //if image is clicked, then change statement and the picture... vice-versa
+
+
+  yesApiImage = () => {
+    return (yes["yes-image"])
+  }
+
+  noApiImage = () => {
+    return (no["no-image"])
+  }
+  
+
+  render() {
+    // console.log({yes, no})
+    return (
+      <>
+        <span>
+          <h1>You Can Do This!</h1>
+        </span>
+        <span>
+          <img src={this.yesApiImage}></img>
+        </span>
+      </>
+    );
+  }
 }
+
+
 
 export default App;
