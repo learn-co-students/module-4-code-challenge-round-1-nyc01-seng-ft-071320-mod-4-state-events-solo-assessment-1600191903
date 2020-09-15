59ad98ed6910b545e96913252d157db6c666e7de
victorscholz <vgscholz@gmail.com>
2020-09-15 14:47:00 -0400

    work completed within the alloted hour

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './App.css';
+import { yes, no } from './objects.js'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+
+  state = {
+    clicked: false
+  }
+
+  clickHandler = () => {
+    console.log("clicked")
+    this.setState((currentState) => ({clicked: !currentState.clicked}))
+  }
+
+  render() {
+    return (
+    <div>
+      <h1>
+        {/* statement */}
+      </h1>
+      <img onClick={this.clickHandler}>
+        {/* img src */}
+      </img>
+    </div>
+    )
+  }
 }
 
 export default App;
