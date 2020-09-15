b44b0e2c054534d682302e8390148e986913a532
jakelozano94 <jakelozano94@gmail.com>
2020-09-15 14:33:41 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import './App.css';
+import {yes, no} from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+
+  state={
+    toggle: false
+  }
+
+  clickHandler = () => {
+    console.log("clicking")
+    this.setState((previousState) => ({ toggle: !previousState.toggle }), () => console.log("State: ", this.state))
+  }
+  
+  render(){
+    // console.log(Object.values(yes))
+    // console.log(this.state)
+    return (
+      <>
+  <h1>You Can Do This!</h1>
+  <img onClick={this.clickHandler} src={this.state.toggle ? Object.values(yes) : Object.values(no)[0]}/>
+  </>
+  )
+
+  }
 }
 
 export default App;
