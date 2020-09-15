551435e76e06653ec0e039680f07655412004251
Se min Lee <mlsml94@gmail.com>
2020-09-15 14:50:04 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { yes, no } from './objects.js';
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends Component {
+
+  state = {
+    src: yes["yes-image"]
+  }
+
+  // yesorno = () => {
+  //   return ({ yes, no })
+  // }
+
+  clickHandler = () => {
+    console.log("clicked")
+    this.setState({ src: no["no-image"]})
+  }
+  
+  render() {
+    return (
+      <div className="card">
+        <div className="statement">
+          <h1>{yes["yes-statement"]}</h1>
+        </div>
+        {/* <div className="image"> */}
+          <img className="image" onClick={this.clickHandler} src={`${yes["yes-image"]}`}/>
+          {/* {this.state.clicked ? <img className="image" src={`${yes["yes-image"]}`} /> : <img className="image" src={`${no["no-image"]}`}/>} */}
+        {/* </div> */}
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
