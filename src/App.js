e8e49dc6967a61e57876281cdf35141692f25baa
woink <wardprice@gmail.com>
2020-09-15 14:49:41 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { yes, no } from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+
+class App extends Component {
+
+  state = {
+    clicked: false
+  }
+
+  clickHandler = () => {
+    this.setState((previousState) => ({clicked: !previousState.clicked}))
+  }
+
+  
+  render() {
+    console.log(this.state)
+    return (
+      
+      <span className='card'>
+        <h1>{yes['yes-statement']}</h1>
+        <img onClick={this.clickHandler} alt='' src={yes['yes-image']} />
+        {this.state.clicked ? <span className='card'> <h1>{no['no-statement']}</h1> <img alt='' src={no["no-image"]}/> </span> : null }
+      </span>
+    )
+  }
 }
 
 export default App;
