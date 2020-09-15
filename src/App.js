4d9d532f19584f6de85c7be08f6277c4dd98335d
amitdesh <amitdesh2014@gmail.com>
2020-09-15 14:22:42 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import './App.css';
+import {yes, no} from './objects.js'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component{
+
+
+  state = {
+    words: yes['yes-statement'],
+    image: yes['yes-image']
+  }
+
+  clickHandler =() =>{
+    if (this.state.words === yes['yes-statement']){
+      this.setState({
+        words: no['no-statement'],
+        image: no['no-image']
+      })
+    }
+    else if (this.state.words === no['no-statement']){
+      this.setState({
+        words: yes['yes-statement'],
+        image: yes['yes-image']
+      })
+    }
+  }
+  
+  
+    render = () => {
+      return(
+        <div>
+          <h2>{this.state.words}</h2>
+          <img onClick={this.clickHandler} src={this.state.image}/>
+          </div>
+      )
+    }
+  
+
 }
+
 
 export default App;
