032c25c77760b762317521d155908be544480144
“jzhouerm” <jzhouerm@hotmail.com>
2020-09-15 14:48:17 -0400

    final push

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import './App.css';
+import {yes, no} from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+
+
+class App extends React.Component {
+
+  state = {
+    clicked: false
+  }
+
+  clickHandler = () => {
+    console.log("State: ", this.state)
+    this.setState((previousState) => ({clicked: !previousState.clicked}))
+  }
+
+  render () {
+    return (
+      <>
+      <h1 onClick={this.ClickHandler}> I'm not sure if I can do this...</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQptvy1IuxkMzOgcsR_uWucIkwE67jI04GfUg&usqp=CAU"></img>
+      </>
+    )
+
+  }
 }
 
 export default App;
