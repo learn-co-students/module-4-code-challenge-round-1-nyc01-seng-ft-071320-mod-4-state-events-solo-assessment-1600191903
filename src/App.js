3060a78014cba6aab377eb262c882c662d08cd54
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-09-15 14:39:07 -0400

    i need to review more

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import yOrN from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+
+
+  render() {
+    return <h1>You Can Do This!</h1>;
+  }
 }
 
 export default App;
--- src/objects.js
+++ src/objects.js
@@ -1,5 +1,16 @@
-const yes = { "yes-image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQptvy1IuxkMzOgcsR_uWucIkwE67jI04GfUg&usqp=CAU", "yes-statement": "google the answer" }
+import React from 'react'
 
-const no = { "no-image": "https://thumbs.gfycat.com/ElaborateShinyBernesemountaindog-size_restricted.gif", "no-statement": "learn problem solving, syntax, reading documentation" }
+class yOrN extends React.Component{
 
-export { yes, no }
+    state = {
+
+    }
+    const yes = { "yes-image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQptvy1IuxkMzOgcsR_uWucIkwE67jI04GfUg&usqp=CAU", "yes-statement": "google the answer" }
+
+    const no = { "no-image": "https://thumbs.gfycat.com/ElaborateShinyBernesemountaindog-size_restricted.gif", "no-statement": "learn problem solving, syntax, reading documentation" } 
+}
+
+
+
+
+export default yOrN
