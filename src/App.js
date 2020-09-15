df1baee5641c4e0695cdb784d7040b5bef48f5bb
pairofAces <karanchauhan16@gmail.com>
2020-09-15 14:42:54 -0400

    Stuck with the api...

--- src/App.js
+++ src/App.js
@@ -16,18 +16,21 @@ class App extends React.Component {
   //if image is clicked, then change statement and the picture... vice-versa
 
   //I can't map over an object since it isn't an array of objects... so then what?
-  
-  yesApiImage = () => {
-    return (yes["yes-image"])
-  }
 
-  noApiImage = () => {
-    return (no["no-image"])
+  // yesApiImage = () => {
+  //   return (yes["yes-image"])
+  // }
+  
+  //playing around with Object.entries() --> to try and make the object into an array
+  // so that I can use .map
+  noApi = () => {
+    no.entries(no).map(item => {console.log(item)})
   }
   
 
   render() {
-    // console.log({yes, no})
+    //the console.logging works! 
+    //console.log({yes, no})
     return (
       <>
         <span>
