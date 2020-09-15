0f2c4da46d6aab1a106f41a4c0b552df341bac07
vincentyang01 <vincentyangcsi@gmail.com>
2020-09-15 15:10:48 -0400

    added statement

--- src/App.js
+++ src/App.js
@@ -20,11 +20,17 @@ class App extends React.Component {
       <div>
         <h1> You can do this!</h1>
         <h2> A random statement. </h2>
-        {/* <img src="https://rb.gy/tewo7n"></img> */}
-        {/* <img alt="Something meaningful" onClick={this.clickHandler}></img> */}
+
         {this.state.clicked 
-            ? <img onClick={this.clickHandler} alt="Line25" src={yes["yes-image"]}></img> 
-            : <img onClick={this.clickHandler} alt="Line25" src={no["no-image"]}></img>}
+            ? <>
+                <h2>{yes["yes-statement"]}</h2>
+                <img onClick={this.clickHandler} alt="Line25" src={yes["yes-image"]}></img> 
+              </>
+            : <>
+                <h2>{no["no-statement"]}</h2>
+                <img onClick={this.clickHandler} alt="Line25" src={no["no-image"]}></img>
+              </>
+        }
       </div>
     )
   }
