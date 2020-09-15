bbaba67c58d63972c9d3789da92856fff001450f
vincentyang01 <vincentyangcsi@gmail.com>
2020-09-15 14:24:29 -0400

    sure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import './App.css';
+import {yes, no} from './objects.js'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+
+class App extends React.Component {
+
+  state = {
+    clicked: true
+  }
+
+  clickHandler = () => {
+    console.log("logged a click")
+    this.setState((previousState) => ({ clicked: !previousState.clicked }))
+  }
+
+  render(){
+    console.log(this.state)
+    return(
+      <div>
+        <h1> You can do this!</h1>
+        <h2> A random statement. </h2>
+        {/* <img src="https://rb.gy/tewo7n"></img> */}
+        {/* <img alt="Something meaningful" onClick={this.clickHandler}></img> */}
+        {this.state.clicked 
+            ? <img onClick={this.clickHandler} alt="Line25" src={yes["yes-image"]}></img> 
+            : <img onClick={this.clickHandler} alt="Line25" src={no["no-image"]}></img>}
+      </div>
+    )
+  }
+
 }
 
 export default App;
