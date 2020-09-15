8bb8a513256bbc9915141d0598cf573716df8c50
taevonjordan11 <taevonjordan1@gmail.com>
2020-09-15 14:47:40 -0400

    render

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
+import { yes, no } from './objects.js'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
-}
+
+
+// function App() {
+//   return <h1>You Can Do This!</h1>;
+// }
+
+
+
+  class App extends React.Component {
+
+    yesStatement = () => {
+      return yes.map(element => key={element.yes_image})
+    }
+
+    render(){
+      return (
+        <>
+        <h2> Something </h2>
+        </>
+      )
+    }
+  }
 
 export default App;
